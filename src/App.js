6c76bbf19e5534a700879237384f206779dc984b
LexElena22 <103533828+LexElena22@users.noreply.github.com>
2022-12-19 12:20:34 -0800

    completed wave 03

--- src/App.js
+++ src/App.js
@@ -1,51 +1,92 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const INITIAL_TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const INITIAL_TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
-  const initialCopy = INITIAL_TASKS.map((task) => {
-    return { ...task };
-  });
-  const [taskList, setTasklist] = useState(initialCopy);
+  // const initialCopy = INITIAL_TASKS.map((task) => {
+  //   return { ...task };
+  // });
+  const [taskList, setTasklist] = useState([]);
+
+  const URL = 'http://localhost:5000/tasks';
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        //console.log(res);
+        const tasksAPIResCopy = res.data.map((task) => {
+          return {
+            ...task,
+          };
+        });
+        setTasklist(tasksAPIResCopy);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   const updateTask = (taskId, updatedStatus) => {
+    let status = 'mark_complete';
+    if (updatedStatus) {
+      status = 'mark_complete';
+    } else {
+      status = 'mark_incomplete';
+    }
     console.log('updateTask called');
     const newTaskList = [];
-    for (const task of taskList) {
-      if (task.id != taskId) {
-        newTaskList.push(task);
-      } else {
-        const newTask = {
-          ...task,
-          isComplete: updatedStatus,
-        };
-        newTaskList.push(newTask);
-      }
-    }
-    setTasklist(newTaskList);
+    axios
+      .patch(`${URL}/${taskId}/${status}`)
+      .then((res) => {
+        for (const task of taskList) {
+          if (task.id != taskId) {
+            newTaskList.push(task);
+          } else {
+            const newTask = {
+              ...task,
+              isComplete: updatedStatus,
+            };
+            newTaskList.push(newTask);
+          }
+        }
+        setTasklist(newTaskList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const deleteTask = (taskId) => {
-    const newTaskList = [];
-    for (const task of taskList) {
-      if (task.id !== taskId) {
-        newTaskList.push(task);
-      }
-    }
-    setTasklist(newTaskList);
+    console.log('deleteTask Called');
+    axios
+      .delete(`${URL}/${taskId}`)
+      .then(() => {
+        const newTaskList = [];
+        for (const task of taskList) {
+          if (task.id !== taskId) {
+            newTaskList.push(task);
+          }
+        }
+        setTasklist(newTaskList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
